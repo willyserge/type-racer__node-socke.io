f06ac9fc7721964af076b9952b3a7093733df088
willyserge <willyserge01@gmail.com>
2020-06-22 18:34:06 +0200

    join existing game

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
 import express from 'express';
 import dotenv from 'dotenv';
@@ -27,6 +28,25 @@ mongoose.connect(process.env.MONGODB_URI,
   });
 
 io.on('connect', (socket) => {
+  socket.on('join-game', async ({ gameID: _id, nickName }) => {
+    try {
+      let game = await Game.findById(_id);
+      if (game.isOpen) {
+        const gameID = game._id.toString();
+        socket.join(gameID);
+        const player = {
+          socketID: socket.id,
+          nickName,
+        };
+        game.players.push(player);
+        game = await game.save();
+        io.to(gameID).emit('updateGame', game);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
   socket.on('create-game', async (name) => {
     try {
       const quotableData = await getData();
@@ -39,7 +59,6 @@ io.on('connect', (socket) => {
       };
       game.players.push(player);
       game = await game.save();
-      // eslint-disable-next-line no-underscore-dangle
       const gameID = game._id.toString();
       socket.join(gameID);
       io.to(gameID).emit('updateGame', game);
